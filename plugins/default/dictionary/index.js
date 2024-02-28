cdbb6dc6a1368fc8f8f6977081e86afbaec80621
Evan Coury <me@evancoury.com>
2024-02-28 12:53:10 -0500

    Added dictionary plugin

---
+++ plugins/default/dictionary/index.js
@@ -0,0 +1,43 @@
+const PluginInterface = require('../../../src/pluginInterface');
+const https = require('node:https');
+
+function doRequest(url) {
+  return new Promise(function (resolve, reject) {
+    https.get(url, function(response) {
+      var data = '';
+      response.on('data', function (chunk) {
+          data += chunk;
+      });
+      response.on('end', async function () {
+        resolve(JSON.parse(data));
+      });
+      response.on('error', async function (error) {
+        reject(error);
+      });
+    });
+  });
+}
+
+class DictionaryPlugin extends PluginInterface {
+  init() {
+    this.__centralEventEmitter.on('messageReceived', this.onMessage.bind(this));
+  }
+
+  async onMessage(msg) {
+    var args    = msg.message.slice(1).split(' ');
+    var command = args.shift().toLowerCase();
+
+    if (command === 'define' && args.length > 0) {
+      var defResponse = await doRequest(`https://api.dictionaryapi.dev/api/v2/entries/en/${args[0]}`);
+      var responseText = `${defResponse[0]['word']} (${defResponse[0]['meanings'][0]['partOfSpeech']}) ${defResponse[0]['phonetic']} - ${defResponse[0]['meanings'][0]['definitions'][0]['definition']}`;
+      msg.twitchClient.say(msg.channel, `${responseText}`);
+      return;
+    }
+  }
+
+  unload() {
+    this.__centralEventEmitter.removeListener('messageReceived', this.onMessage);
+  }
+}
+
+module.exports = DictionaryPlugin;
